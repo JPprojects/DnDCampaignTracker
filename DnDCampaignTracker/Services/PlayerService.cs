60d0bbe9f7facb03580cf4153cbe1306a2dd43db
JPprojects <jerome791@hotmail.co.uk>
2024-09-17 16:49:15 +0100

    update pages so index submits the campaign to the players page

--- DnDCampaignTracker/Services/PlayerService.cs
+++ DnDCampaignTracker/Services/PlayerService.cs
@@ -5,14 +5,14 @@ namespace DnDCampaignTracker.Services;
 
 public class PlayerService:BaseService
 {
-    public async Task<List<PlayersModel>> AsyncBuildPlayersModel()
+    public async Task<List<PlayersModel>> AsyncBuildPlayersModel(string campaign)
     {
         var model = await DeserializeJsonAsync<PlayersModel>("Players");
         var playerList = new List<PlayersModel>();
 
         foreach (var player in model)
         {
-            if (player.Campaign == "Divine Titans")
+            if (player.Campaign == campaign)
             {
                 var viewModel = new PlayersModel
                 {
