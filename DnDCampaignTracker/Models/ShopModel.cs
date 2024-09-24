869588ada9f12034d50ad273996421621ea67c95
JPprojects <jerome791@hotmail.co.uk>
2024-09-24 11:09:03 +0100

    Add locations view with page model incorporated

---
+++ DnDCampaignTracker/Models/ShopModel.cs
@@ -0,0 +1,10 @@
+﻿namespace DnDCampaignTracker.Models;
+
+public class ShopModel
+{
+    public string Name { get; set; }
+    public string Owner { get; set; }
+
+    public List<string> Items { get; set; }
+}
+
---
+++ DnDCampaignTracker/Pages/Locations.cshtml.cs
@@ -0,0 +1,71 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using DnDCampaignTracker.Models;
+using Newtonsoft.Json;
+namespace DnDCampaignTracker.Pages;
+
+public class LocationsModel : PageModel
+{
+    public string Name { get; set; }
+    public string Country { get; set; }
+    public string Description { get; set; }
+
+    public List<ShopModel> Shops { get; set; }
+
+    public async Task OnGet()
+    {
+        string filePath = Path.Combine(Directory.GetCurrentDirectory(), "Data", "locations.json");
+
+        if (!System.IO.File.Exists(filePath))
+        {
+            // Handle the case where the file is not found
+            ModelState.AddModelError("", "Location data not found.");
+        }
+        else
+        {
+            var jsonData = await System.IO.File.ReadAllTextAsync(filePath);
+
+            var locationData = JsonConvert.DeserializeObject<List<LocationModel>>(jsonData);
+
+            if (locationData != null)
+            {
+                foreach (var location in locationData)
+                {
+                    // Assign the deserialized values to the model properties
+                    Name = location.Name;
+                    Country = location.Country;
+                    Description = location.Description;
+                    Shops = location.Shops;
+                }
+            }
+        }
+    }
+
+    // Define the LocationModel class (one single class for the location data)
+    public class LocationModel
+    {
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public string Description { get; set; }
+        public List<ShopModel> Shops { get; set; } // List of shops within the location
+    }
+
+    // Define the ShopModel class to represent individual shops
+    public class ShopModel
+    {
+        public string Name { get; set; }
+        public string Owner { get; set; }
+
+        public List<string> Items { get; set;}
+
+    }
+}
+
+
+//[Location.cshtml] <-- (View)
+//    |
+//    v
+//[LocationsModel.cs] <-- (PageModel - Handles logic, loads data)
+//    |
+//    v
+//[LocationModel.cs] <-- (Data Model - Holds structured data)
